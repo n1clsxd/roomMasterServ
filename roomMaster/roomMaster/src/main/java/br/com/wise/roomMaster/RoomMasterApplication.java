231c927cbb5686804c58e6c38c351899d1c086ff
Nicolas Mickus <nicolasmickus@gmail.com>
2020-03-06 13:19:27 -0300

    Upload Inicial

---
+++ roomMaster/roomMaster/src/main/java/br/com/wise/roomMaster/RoomMasterApplication.java
@@ -0,0 +1,13 @@
+package br.com.wise.roomMaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomMasterApplication.class, args);
+	}
+
+}
---
+++ roomMaster/roomMaster/src/test/java/br/com/wise/roomMaster/RoomMasterApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.wise.roomMaster;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RoomMasterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
